382aa64bc5b75a1d47ebdb5041e3e90c5eaeeaae
longhike <sam.ascheim@gmail.com>
2021-02-28 18:16:16 -0500

    replacing script.js file with the version we did in class

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-var wins = 0;
-var ties = 0;
-var losses = 0;
+//                      0    1    2
+let possibleAnswers = ["r", "p", "s"];
 
-// Array of options for computer to pick from
-var options = ["R", "P", "S"];
+let myScore = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  };
 
-var playGame = function() {
-  // Ask user for their choice
-  var userChoice = window.prompt("Enter R, P, or S:");
+function runGame() {
+      let myInput = window.prompt("Do you want to enter r p or s (or q to quit):");
+      if (myInput === "q") {
+          window.alert("Ok, bye!")
+      } else {
+          round(myInput)
+      }      
+}
 
-  // If user pressed Cancel, immediately end function
-  if (!userChoice) {
-    return;
+runGame()
+
+function round(functionMyInput) { 
+    let randomNum = Math.floor(Math.random() * 3);
+    let compInput = possibleAnswers[randomNum];
+    if (functionMyInput === compInput) {
+      myScore.ties = myScore.ties + 1;
+    } else {
+      if (functionMyInput === "r" && compInput === "p") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.losses = myScore.losses + 1;
+      }
+      if (functionMyInput === "r" && compInput === "s") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.wins = myScore.wins + 1;
+      }
+      if (functionMyInput === "p" && compInput === "s") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.losses = myScore.losses + 1;
+      }
+      if (functionMyInput === "p" && compInput === "r") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.wins = myScore.wins + 1;
+      }
+      if (functionMyInput === "s" && compInput === "r") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.losses = myScore.losses + 1;
+      }
+      if (functionMyInput === "s" && compInput === "p") {
+          console.log(`my input: ${functionMyInput}`)
+          console.log(`comp input: ${compInput}`)
+          myScore.wins = myScore.wins + 1;
+      }
+    }
+    window.alert(`wins: ${myScore['wins']}\nlosses: ${myScore['losses']};\nties: ${myScore['ties']}`)
+    runGame()
   }
-
-  // Convert to uppercase to make comparisons easier
-  userChoice = userChoice.toUpperCase();
-
-  // Get random index from array of options
-  var index = Math.floor(Math.random() * options.length);
-  var computerChoice = options[index];
-
-  window.alert("The computer chose " + computerChoice);
-
-  // If choices are the same, it's a tie
-  if (userChoice === computerChoice) {
-    ties++;
-    window.alert("It's a tie!");
-
-  // Check every win condition for the player
-  } else if (
-    (userChoice === "R" && computerChoice === "S") || 
-    (userChoice === "P" && computerChoice === "R") || 
-    (userChoice === "S" && computerChoice === "P")
-  ) {
-    wins++;
-    window.alert("You win!");
-
-  // If above conditions failed, assume player lost
-  } else {
-    losses++;
-    window.alert("You lost!");
-  }
-
-  // Print stats with line breaks
-  window.alert(
-    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
-  );
-
-  // Ask user to play again
-  var playAgain = window.confirm("Play again?");
-
-  // If user pressed OK, run the function again
-  if (playAgain) {
-    playGame();
-  }
-};
-
-// Run the game for the first time
-playGame();
